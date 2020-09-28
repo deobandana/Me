f4ac63007b2ea4b4fa3e7b8d5763de6dc10d32c2
deobandana <deobandana1@gmail.com>
2020-09-27 23:34:25 -0400

    fdfd

--- static/js/app.js
+++ static/js/app.js
@@ -302,9 +302,9 @@ d3.json("data/samples.json").then((dataset) => {
     var userSample = inputElement.property("value").replace('BB_', '');
 
     //Acquire the the value property of the radio input
-    /* checkedValue = getCheckedRadioValue("type");
+    checkedValue = getCheckedRadioValue("type");
 
-    if (checkedValue==='bar'){ */
+    if (checkedValue==='bar'){
     //show the bar chart
       showbarChart(sample, userSample);
   /*   } else { */
